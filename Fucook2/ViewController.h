e691c568e3e2f9d6b653453fad4d9346662c7447
Hugo Costa <hugocosta@goodpeople.com>
2014-12-09 09:38:47 +0000

    Initial Commit

---
+++ Fucook2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fucook2
+//
+//  Created by Hugo Costa on 09/12/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fucook2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fucook2
+//
+//  Created by Hugo Costa on 09/12/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
